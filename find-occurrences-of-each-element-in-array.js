28bd909577f48c8b14545ee7c7b6c9a2479231af
Salman Arefin <45092817+salman-arefin74@users.noreply.github.com>
2022-04-21 19:33:33 +0000

    Refactored.

--- find-occurrences-of-each-element-in-array.js
+++ find-occurrences-of-each-element-in-array.js
@@ -1,3 +1,7 @@
 const occurrences = tasks.reduce(function (acc, curr){
   return acc[curr] ? ++acc[curr] : acc[curr] = 1, acc
 }, {});
+
+for (var key of Object.keys(occurrences)) {
+  console.log(occurrences[key]);
+}
